e00f5dcadd3940ba33bdd3935bcba0baa7b9a58d
naoko1204 <vladlena.popovici00@gmail.com>
2023-06-14 00:00:59 +0300

    Implemented gravity and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,48 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 5f;
-    [SerializeField] private float _runSpeedMultiplier = 2f;
-    [SerializeField] private float _jumpForce = 3f;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float groundYOffset;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float runSpeedMultiplier = 2f;
 
-    private bool _isJumping;
-    private bool _isRunning;
     private CharacterController _characterController;
+    private Vector3 _movement;
+    private Vector3 _spherePosition;
+    private Vector3 _velocity;
+    private bool _isRunning;
 
-    // Start is called before the first frame update
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         HandleMovementInput();
         HandleJumpInput();
-        HandleRunInput();
-    }
+        // HandleRunInput();
 
-    private void HandleRunInput()
-    {
-        
-    }
-
-    private void HandleJumpInput()
-    {
-        
+        ApplyGravity();
     }
 
     private void HandleMovementInput()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput) * (_moveSpeed * Time.deltaTime);
+        _movement = transform.forward * verticalInput + transform.right * horizontalInput;
+        // FIXME diagonal speed
 
-        _characterController.Move(direction);
+        _characterController.Move(_movement * (moveSpeed * Time.deltaTime));
+    }
+
+    private void HandleJumpInput()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space) || !IsGrounded()) return;
+
+        _velocity.y += jumpForce;
+        _characterController.Move(_velocity * Time.deltaTime);
+    }
+
+    // private void HandleRunInput()
+    // {
+    //     
+    // }
+
+    private void ApplyGravity()
+    {
+        if (!IsGrounded()) _velocity.y += gravity * Time.deltaTime;
+        else if (_velocity.y < 0) _velocity.y = -2f;
+
+        _characterController.Move(_velocity * Time.deltaTime);
+    }
+
+    private bool IsGrounded()
+    {
+        var position = transform.position;
+        _spherePosition = new Vector3(position.x, position.y - groundYOffset, position.z);
+        return Physics.CheckSphere(_spherePosition, _characterController.radius - 0.05f, groundMask);
     }
 }
