a449dffd36cd3b5eff34df4137c98f133c14aeff
naoko1204 <vladlena.popovici00@gmail.com>
2023-06-16 12:19:19 +0300

    Added main menu and bussines logic for buttons

---
+++ Assets/Scripts/UI/MainMenuLogic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenuLogic : MonoBehaviour
+    {
+        public void StartButton()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void QuitButton()
+        {
+            Application.Quit();
+            Debug.Log("Quit");
+        }
+    }
+}
